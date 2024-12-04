1a31801b77536801772720871acddc60da4cca3a
laitaton <91537360+laitaton@users.noreply.github.com>
2024-12-04 15:07:09 +0200

    Modal backdrop opacity in dark theme

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -10,7 +10,7 @@ interface ModalProps {
 
 export default function Modal(props: ModalProps): ReturnType<FC> {
   return (
-    <div onClick={props.onClose} className={`${"fixed z-40 inset-0 w-full h-full bg-black/25"} ${props.open ? "block" : "hidden"}`}>
+    <div onClick={props.onClose} className={`${"fixed z-40 inset-0 w-full h-full bg-black/25 dark:bg-black/75"} ${props.open ? "block" : "hidden"}`}>
       <div className="fixed z-50 p-lg bg-theme-modal-background w-[35rem] h-auto top-[40%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] flex flex-col items-center rounded-[20px]">
         <div className="flex flex-row w-full justify-between select-none">
           <h2 className="text-primary text-2xl font-primary font-bold ">{props.header}</h2>
