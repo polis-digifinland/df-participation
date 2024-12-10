26952803b5f3d477bcb498de18cf5f5f60a7a755
laitaton <91537360+laitaton@users.noreply.github.com>
2024-12-10 12:22:01 +0200

    Update copy-pages.sh

--- scripts/copy-pages.sh
+++ scripts/copy-pages.sh
@@ -3,8 +3,11 @@
 # chmod +x ./scripts/copy-pages.sh
 # Usage: ./scripts/copy-pages.sh
 
-cp src/app/[id]/df/page.tsx src/app/[id]/aqua/page.tsx
-cp src/app/[id]/df/page.tsx src/app/[id]/basic/page.tsx
-cp src/app/[id]/df/page.tsx src/app/[id]/cappuccino/page.tsx
-cp src/app/[id]/df/page.tsx src/app/[id]/dark/page.tsx
+BASE_PATH="src/app/[id]"
+BASE_THEME="$BASE_PATH/df/page.tsx"
+
+cp $BASE_THEME $BASE_PATH/aqua/page.tsx
+cp $BASE_THEME $BASE_PATH/basic/page.tsx
+cp $BASE_THEME $BASE_PATH/cappuccino/page.tsx
+cp $BASE_THEME $BASE_PATH/dark/page.tsx
 
