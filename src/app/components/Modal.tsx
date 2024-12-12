56734a7b1eb4d05200443bd126d01768451ac348
laitaton <91537360+laitaton@users.noreply.github.com>
2024-12-12 17:43:46 +0200

    Fix modal on mobile

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -32,7 +32,7 @@ export default function Modal({ open, onClose, header, children }: ModalProps): 
 
   return (
     <div onClick={onClose} className={`${"fixed z-40 inset-0 w-full h-full bg-black/25 dark:bg-black/75"} ${open ? "block" : "hidden"}`}>
-      <div role="dialog" aria-modal="true" className="fixed z-50 p-lg bg-theme-modal-background w-[35rem] h-auto top-[40%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] flex flex-col items-center rounded-[20px]">
+      <div role="dialog" aria-modal="true" className="fixed z-50 p-lg bg-theme-modal-background lg:w-[35rem] h-auto top-[40%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] flex flex-col items-center rounded-[20px]">
         <div className="flex flex-row w-full justify-between select-none">
           <h2 className="text-primary text-2xl font-primary font-bold ">{header}</h2>
             <button ref={closeButtonRef} onClick={onClose} className='rounded-md hover:lg:scale-110 active:scale-110' aria-label="Sulje"><Close fg="var(--text-primary)" /></button>
