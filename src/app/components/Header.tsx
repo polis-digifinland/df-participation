0cabdcc1d84431c3f1d014fbb8eff80a914f35f9
laitaton <91537360+laitaton@users.noreply.github.com>
2024-12-12 19:33:37 +0200

    Added language dropdown

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,10 +1,12 @@
 import Logo from '../icons/Logo';
+import LanguageChanger from '@/components/LanguageChanger';
 
 export default function Header() {
 	return (
 		<>
-			<div id="Header" className="flex flex-row mx-auto mt-xl px-md max-w-screen-sm select-none w-full">
-                <Logo  fg="var(--logo-primary)" bg="var(--logo-secondary)" />
+			<div id="Header" className="flex flex-row justify-between mx-auto mt-xl px-md max-w-screen-sm select-none w-full">
+        <Logo  fg="var(--logo-primary)" bg="var(--logo-secondary)" />
+				<LanguageChanger />
 			</div>
 		</>
 	)
---
+++ src/app/components/LanguageChanger.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
+import { useTranslation } from 'react-i18next';
+import { ChangeEvent } from 'react';
+
+export default function LanguageChanger() {
+  const { i18n } = useTranslation();
+  const currentLocale = i18n.language;
+  const router = useRouter();
+  const currentPathname = usePathname();
+
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const newLocale = e.target.value;
+
+    router.replace(
+        currentPathname.replace(`/${currentLocale}/`, `/${newLocale}/`)
+      );
+
+      i18n.changeLanguage(newLocale);
+  };
+
+  return (
+    <select onChange={handleChange} value={currentLocale} className="text-primary font-secondary">
+      <option value="fi">Suomi</option>
+      <option value="sv">Svenska</option>
+      <option value="en">English</option>
+    </select>
+  );
+}
